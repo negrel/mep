a34451233ad5ddfc19153075c41854ea1d4a8ced
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-09 22:43:22 +0100

    adding test for parenthesis

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -15,8 +15,34 @@ describe('Parser', () => {
   })
 
   it('multiple operation with parenthesis', () => {
+    const result = Parser.parse('(1 + 3) * 5 + 2')
+
+    expect(result).toEqual([1, 3, OPERATIONS.get('+'), 5, OPERATIONS.get('*'), 2, OPERATIONS.get('+')])
+  })
+
+  it('multiple operation with multiple parenthesis', () => {
     const result = Parser.parse('(1 + 3) * (5 + 2)')
 
     expect(result).toEqual([1, 3, OPERATIONS.get('+'), 5, 2, OPERATIONS.get('+'), OPERATIONS.get('*')])
+  })
+
+  it('multiple operation with nested parenthesis', () => {
+    const result = Parser.parse('1 + 2 * (3 * (5 + 2)) + -9 * 3')
+
+    expect(result).toEqual([
+      1,
+      2,
+      3,
+      5,
+      2,
+      OPERATIONS.get('+'),
+      OPERATIONS.get('*'),
+      OPERATIONS.get('*'),
+      OPERATIONS.get('+'),
+      -9,
+      3,
+      OPERATIONS.get('*'),
+      OPERATIONS.get('+')
+    ])
   })
 })
